3b41d41dc036ff14038e46dc6d63d039b8905837
bryanthayes <bryantfhayes@gmail.com>
2017-04-21 23:33:30 -0700

    update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='bhutils',
-      version='0.1',
+      version='1.0.1',
       description='A python package for cool python stuff!',
       url='http://github.com/bryantfhayes/bhutils',
       author='Bryant Hayes',
